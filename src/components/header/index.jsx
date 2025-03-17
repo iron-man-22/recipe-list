9e16ec2cc13a07fa5ac691b0c5dea4c27d253099
Gangadhar <gangadharrock220@gmail.com>
2025-03-17 10:19:02 +0530

    Update index.jsx , Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,7 @@
 
 function Header(){
     return (<header>
-    <div>Home Page</div>
-    <ul style={{listStyle:'none'}}>
-        <li>Recipes</li>
-        <li>Comments</li>
-    </ul>
+     
     </header>
     );
 }
